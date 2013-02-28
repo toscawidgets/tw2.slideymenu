be880030bd1e0b78dad66fdae034a84e999eb632
Ralph Bean <rbean@redhat.com>
2013-02-27 21:56:50 -0500

    2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ _extra_mako = ["Mako >= 0.1.1"]
 
 setup(
     name='tw2.slideymenu',
-    version='2.1',
+    version='2.2',
     description='toscawidgets2 wrapper for random filament group tools',
     long_description=long_description,
     author='Ralph Bean',
